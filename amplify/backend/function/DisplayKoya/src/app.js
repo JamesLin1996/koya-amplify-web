25ec7b7ad700fb937385b42a3a504573168f84eb
JamesLin1996 <james.lin@utexas.edu>
2021-01-09 22:01:59 -0600

    added get presign endpoint

--- amplify/backend/function/DisplayKoya/src/app.js
+++ amplify/backend/function/DisplayKoya/src/app.js
@@ -25,6 +25,14 @@ app.use(function(req, res, next) {
 // KOYA database
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
+const s3 = new AWS.S3({
+  endpoint: 's3-us-west-2.amazonaws.com',
+  accessKeyId: 'AKIAQMNCMWCXR2YPI5NO',
+  secretAccessKey: 'AfVoOu9Ga7tkQzoueoTnC/tsEn2hdFNvegGmTK57',
+  Bucket: 'koya-web',
+  signatureVersion: 'v4',
+  region: 'us-west-2'
+});
 
 /**********************
  * Example get method *
@@ -35,9 +43,8 @@ app.get('/koya/:id', function(req, res) {
   const params = {
     TableName: 'koya',
     Key: {'submission_id': req.params.id}   // 5fdc7459ffdb4b408c49fc33
-  }
+  };
 
-  console.log('pre get');
   ddb.get(params, function(err, data) {
     if (err) {
       console.log("Error", err);
@@ -49,6 +56,51 @@ app.get('/koya/:id', function(req, res) {
     }
   });
 });
+
+app.get('/presign/:id', function(req, res) {
+  console.log('GET Reached');
+  const params = {
+    TableName: 'koya',
+    Key: {'submission_id': req.params.id}   // 5fe53eadd0893b23335205f4
+  };
+
+  ddb.get(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+      res.json({fail: 'GET KOYA fail, invalid KOYA id!', url: req.url, id: req.params.id});
+    }
+    else {
+      console.log("Success", data.Item);
+      // special case when video or photo is uploaded
+      if ('image' in data.Item) {
+        const s3_key = data.Item.image.split('.amazonaws.com/koya-web/')[1];
+        res.json({success: 'GET KOYA success!', data: getSignedUrl(s3_key)});
+      }
+      else if ('video' in data.Item) {
+        const s3_key = data.Item.video.split('.amazonaws.com/koya-web/')[1];
+        res.json({success: 'GET KOYA success!', data: getSignedUrl(s3_key)});
+      }
+      else {
+        res.json({success: 'GET KOYA success!'});
+      }
+    }
+  });
+});
+
+function getSignedUrl(key) {
+  const params = {
+    Bucket: 'koya-web',
+    Key: key,
+    Expires: 60 * 60
+  };
+  try {
+    return s3.getSignedUrl('getObject', params);
+  } catch (err) {
+    if (err) {
+      console.log(err);
+    }
+  }
+}
 
 app.get('/koya/:id/*', function(req, res) {
   // Add your code here
